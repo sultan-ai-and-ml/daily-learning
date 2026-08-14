f2df34518131a3858e7cf8e9ed6f213f705fd1be
Sultan Khan <sultankhan9792750730@gmail.com>
2026-08-14 15:42:08 +0530

    Completion of swapping:

---
+++ cpp/swapping.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter two numbers: \n";
+    cin >> a >> b;
+    cout << "Before swapping: a = " << a << ", b = " << b << endl;
+
+    int temp = a;
+    a = b;
+    b = temp;
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+    return 0;
+}
